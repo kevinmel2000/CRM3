0399dcc46ffb02e6b5987445ed0e7320fafd58fd
LAPTOP-PMQPEEI6\ezeelinkindonesia <teddy@ezeelink.co.id>
2019-01-10 09:45:36 +0700

    FIRST COMMIT

---
+++ perfex_crm/application/views/themes/perfex/views/privacy_policy.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+<div class="panel_s">
+    <div class="panel-body">
+        <h4><?php echo _l('privacy_policy'); ?></h4>
+        <hr />
+        <div class="tc-content">
+            <?php echo $policy; ?>
+        </div>
+    </div>
+</div>
---
+++ perfex_crm/application/views/themes/perfex/views/terms_and_conditions.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+<div class="panel_s">
+    <div class="panel-body">
+        <h4><?php echo _l('terms_and_conditions'); ?></h4>
+        <hr />
+        <div class="tc-content">
+            <?php echo $terms; ?>
+        </div>
+    </div>
+</div>
